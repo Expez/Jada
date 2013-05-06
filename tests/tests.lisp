d3b444971ca5445e734f815e9a7a4dbd392703e1
expez <expez@expez.com>
2013-05-06 17:21:55 +0200

    Most tests inside the jada package.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -1,18 +1,11 @@
 (in-package :jada)
 
-(defpackage #:jada-tests
-  (:use #:common-lisp #:jada #:5am))
-
-(in-package #:jada-tests)
-
 (def-suite jada)
-
-(in-package :jada-tests)
 
 (in-suite jada)
 
 (test create-log-weight-command
-      (let ((cmd (jada::create-log-weight-command "weight 83")))
-        (is (= (jada::weight cmd) 83))))
+      (let ((cmd (create-log-weight-command "weight 83")))
+        (is (= (weight cmd) 83))))
 
 (run!)
