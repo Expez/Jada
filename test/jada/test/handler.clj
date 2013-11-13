05aae3fd2a9a0b570ed8bc839e303378899adb09
expez <expez@expez.com>
2013-11-13 13:55:15 +0100

    Restart with clojure

---
+++ test/jada/test/handler.clj
@@ -0,0 +1,14 @@
+(ns jada.test.handler
+  (:use clojure.test
+        ring.mock.request  
+        jada.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+  
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= (:status response) 404)))))
