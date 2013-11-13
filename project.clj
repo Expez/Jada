0a91ac58911ca795dabd0e39288ebc635f63036e
expez <expez@expez.com>
2013-11-13 17:46:29 +0100

    Add license

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject jada "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Jada is just another diet aid"
+  :url "https://github.com/expez/jada"
+  :license {:name "MIT" :url "http://opensource.org/licenses/MIT" }
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.8"]]
