1e141413f266175d307c9917d94ff1b53238557d
expez <expez@expez.com>
2013-11-13 17:46:54 +0100

    Food can be combined

---
+++ src/jada/core.clj
@@ -0,0 +1,11 @@
+(ns jada.core)
+
+(defrecord Food [name kcal prot fat carbs fiber])
+
+(defn add
+  "Add Food items"
+  [& foods]
+  (letfn [(add [{k1 :kcal p1 :prot f1 :fat c1 :carbs fi1 :fiber}
+                {k2 :kcal p2 :prot f2 :fat c2 :carbs fi2 :fiber}]
+            (Food. "" (+ k1 k2) (+ p1 p2) (+ f1 f2) (+ c1 c2) (+ fi1 fi2)))]
+      (reduce add foods)))
---
+++ test/jada/test/core.clj
@@ -0,0 +1,14 @@
+(ns jada.test.core
+  (:use [jada.core :as core]
+        jada.test.core
+        clojure.test)
+  (:import [jada.core Food]))
+
+(def f1 (Food. "f1" 1000 50 25 75 2))
+(def f2 (Food. "f2" 2000 70 5 5 3))
+(def f3 (Food. "f3" 100 7 2 1 0))
+(def f4 (Food. "" 3100 127 32 81 5))
+
+(deftest add-food
+  (is (= f4 (core/add f1 f2 f3))
+      "each component added"))
