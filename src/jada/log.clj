6b4cf774632c560297c12274ef8b856abd8a3b68
expez <expez@expez.com>
2014-01-06 12:14:47 +0100

    wip commit

--- src/jada/log.clj
+++ src/jada/log.clj
@@ -5,18 +5,18 @@
 (def *log* {})
 
 (defprotocol Log
-  (save [] "Stores the log through some means of persistance.")
-  (load [] "Reads the log back in.")
-  (weight [weight date] "Sets the `weight' of the entry matching `date'.")
-  (eat [food])
-  (eat [food amount] "Log that we ate `amount' of `food'.")
-  (barf [food])
-  (barf [food amount]) "Undo the eating of `amount' of `food'.")
+  (save [this] "Stores the log through some means of persistance.")
+  (restore [this] "Reads the log back in.")
+  (weight [this weight date] "Sets the `weight' of the entry matching `date'.")
+  (eat [this food])
+  (eat [this food amount] "Log that we ate `amount' of `food'.")
+  (barf [this food])
+  (barf [this food amount]) "Undo the eating of `amount' of `food'.")
 
 (defn weight
-  "Sets the weight of the LogEntry, or if only one argument is
-provided the weight of the most recent LogEntry."
-  ([log-entry weight]
+  "Sets the weight for the given `date', or if only two arguments are
+provided the weight of the most recent entry in the log."
+  ([log weight date]
      (assoc log-entry :weight weight))
   ([weight]
     (set! *log* (assoc (first *log*) [:weight weight]))
