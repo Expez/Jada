e682cd2c31037e2d95a19db29f1a6851a7d87cbd
expez <expez@expez.com>
2014-01-09 14:52:00 +0100

    Add clj-time as dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/expez/jada"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT" }
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [clj-time "0.6.0"]]
   :plugins [[lein-ring "0.8.8"]]
   :ring {:handler jada.handler/app}
   :profiles
