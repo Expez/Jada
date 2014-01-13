af67dcbfd4d46bd8ada361b39b42b3025c901839
expez <expez@expez.com>
2014-01-13 14:46:16 +0100

    more tests for food/add

--- test/jada/food_test.clj
+++ test/jada/food_test.clj
@@ -7,10 +7,15 @@
 (def f3 (create "f3" 100   7   2  1 0))
 (def f4 (create ""   3100 127 32 81 5))
 (def f5 (create ""   1000 20 -20 -70 1))
+(def f6 (create ""   3000 120 30 80 5))
 
-(deftest add-food
-  (is (= f4 (add f1 f2 f3))
-      "each component added"))
+(testing "add-food"
+  (deftest two
+    (is (= (add f1 f2)
+           f6)))
+  (deftest several
+   (is (= f4 (add f1 f2 f3))
+       "each component added")))
 
 (deftest food-difference
   (is (= (diff f2 f1)
