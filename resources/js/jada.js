5668b130992e1734eff85290913343a0c3cf0f4a
expez <expez@expez.com>
2014-01-16 16:43:04 +0100

    update js to reflect new usage patterns

--- resources/js/jada.js
+++ resources/js/jada.js
@@ -1,17 +1,30 @@
 var ws = new WebSocket("ws://127.0.0.1:8080/websocket");
 ws.onmessage = function(event) {
   var message = JSON.parse(event.data);
-  var html = JSON.parse(message.html);
-  console.log("html = ", html);
+  var action = message.action;
   var recipient = message.recipient;
-  $(recipient).html(html);
+  var value = message.value;
+  if(action === "replace") {
+    $(recipient).val(value);
+  }
 };
 
-$("#box").submit(function(event) {
-  event.preventDefault();
-  var sender = "#box";
-  var val = $("#input").val();
-  ws.send(JSON.stringify({fn: "box", args: val, sender: sender}));
+$('#weight').keypress(function (e) {
+  if (e.which == 13) {
+    e.preventDefault();
+    console.log("1");
+    var sender = "#weight-form";
+    console.log("2");
+    var val = $("#weight").val();
+    console.log("3");
+    if(!val){
+      console.log("4");
+      return;
+    }
+    console.log("sending val: " + val);
+    ws.send(JSON.stringify({fn: "weight", args: val, sender: sender}));
+    return false;    //<---- Add this line
+  }
 });
 ws.onopen = function() {
 };
