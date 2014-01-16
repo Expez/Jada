be2dcb448ceecbd8a3dc041543421beefcf229e4
expez <expez@expez.com>
2014-01-16 16:42:34 +0100

    handler for weight

--- src/jada/handlers.clj
+++ src/jada/handlers.clj
@@ -1,7 +1,11 @@
 (ns jada.handlers
   (:require [jada.html :as html]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [jada.log :as log]))
 
-(defn box [val recipient]
-  {:html (json/write-str (html/inner-box val))
-   :recipient recipient})
+(defn weight [weight recipient]
+  (let [weight (Double/parseDouble weight)]
+    (log/save (log/weight (log/get) weight))
+    {:action :replace
+     :value weight
+     :recipient recipient}))
