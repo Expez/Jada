78d0b3b9fb62dbc89d0c3c1a3968406ea9c80ec0
expez <expez@expez.com>
2014-01-17 12:36:50 +0100

    Add monger as dependency

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
                  [hiccup "1.0.4"]
+                 [com.novemberain/monger "1.7.0"]
                  [org.webbitserver/webbit "0.4.15"]
                  [clj-time "0.6.0"]])
