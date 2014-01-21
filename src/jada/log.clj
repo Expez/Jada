ca47b1c9b5cef9b40463374fca28204460a62a0e
expez <expez@expez.com>
2014-01-21 14:28:00 +0100

    wip commit, moving more code to use mongo

--- src/jada/log.clj
+++ src/jada/log.clj
@@ -7,27 +7,32 @@
             [monger.collection :as mc]
             [monger.joda-time]))
 
-;;; a log is of the form {clj-time.DateTime <LogEntry>}.
 ;;; a logentry has the following keys: weight, foods, bmr, plan and kind.
 ;;; foods is a seq of [food-name amount]
 ;;; plan indicates the diet plan we're following e.g leangains, warriordiet etc
 ;;; kind is a keyword indicating what kind of day this is e.g. :training or
 ;;; :rest, these are often plan specific
 
-(defn entry-for
-  "Returns the log entry for `date'"
-  [date]
-  (let [entry (mc/find-maps "log" {:date date})]
-    (if-not (empty? entry)
-      (first entry)
-      nil)))
 
-(defn- new
+(defn update-in! [date [k & ks] fn & args]
+  "Updates the log entry for `date'"
+  (let [entry (entry-for date)
+        new-entry (update-in entry (list* k ks) fn args)]
+    (mc/update "log" {:date date} new-entry)))
+
+(defn assoc-in! [date [k & ks] v]
+  (let [entry (entry-for date)
+        new-entry (assoc-in entry (list* k ks) value)]
+    (mc/update "log" {:date date} new-entry)))
+
+(defn- new-entry
   "Creates a new log entry for `date' or for today.  Using default
-values from yesterday"
-  ([] (empty (t/today-at-midnight)))
+values from the previous day."
+  ([] (new-entry (t/today-at-midnight)))
   ([date]
-     (let [yesterday (entry-for (t/minus (t/today-at-midnight) (t/days 1)))
+     (let [yesterday (mc/find-one-as-map "log" {:date (t/minus
+                                                       (t/today-at-midnight)
+                                                       (t/days 1))})
            entry {:weight 0
                   :foods []
                   :bmr (:bmr yesterday)
@@ -36,28 +41,34 @@ values from yesterday"
                   :date date}]
        (mc/insert-and-return "log" entry))))
 
+(defn entry-for
+  "Returns the log entry for `date'"
+  [date]
+  (if-let [entry (mc/find-one-as-map "log" {:date date})]
+    entry
+    (new-entry date)))
+
 (defn weight
   "Sets the weight for the given `date', or if only two arguments are
 provided the weight of the most recent entry in the log."
-  ([log date weight]
-     (assoc-in log [date :weight] weight))
-  ([log weight]
-     (assoc-in log [(t/today-at-midnight) :weight] weight))
-  ([weight] (throw (RuntimeException. "Not implemented"))))
+  ([w]
+     (weight (t/today-at-midnight) weight))
+  ([date w]
+     (assoc-in! date [:weight] weight)))
 
 (defn ate
   "Log that we ate `food', or that we ate `amount' of `food'."
-  ([log food]
-     (ate log food 1))
-  ([log food amount]
-     (update-in log [(t/today-at-midnight) :foods] conj [food amount])))
+  ([date food]
+     (ate date food 1))
+  ([date food amount]
+     (update-in! date [:foods] conj [food amount])))
 
 (defn barfed
   "The reverse the eating of `food'."
-  ([log food]
-     (barfed log food 1))
-  ([log food amount]
-     (update-in log [(t/today-at-midnight) :foods]
+  ([date food]
+     (barfed date food 1))
+  ([date food amount]
+     (update-in! date [:foods]
                 (partial remove #{[food amount]}))))
 
 (defn today []
@@ -66,18 +77,12 @@ provided the weight of the most recent entry in the log."
 
 (defn eaten
   "Tallies up all the food items we've eaten on `date' or today."
-  ([log] (eaten log (t/today-at-midnight)))
-  ([log date]
-     (if-let [day (log date)]
-       (reduce f/add (map #(f/mult (first %) (second %)) (:foods day)))
+  ([] (eaten (t/today-at-midnight)))
+  ([date]
+     (if-let [foods (:foods (entry-for date))]
+       (reduce f/add (map #(f/mult (first %) (second %)) foods))
        (f/create "" 0 0 0 0 0))))
 
-(defn set-bmr [log bmr]
-  "Sets the basal metabolic rate in the log."
-  (assoc-in log [t/today-at-midnight :bmr] bmr))
-
-(defn update-in! [date [k & ks] fn & args]
-  "Updates the log entry for `date'"
-  (let [entry (entry-for date)
-        new-entry (update-in entry (list* k ks) fn args)]
-    (mc/update "log" new-entry)))
+(defn set-bmr [date bmr]
+  "Sets the basal metabolic rate in the log, for `date'."
+  (assoc-in! date [:bmr] bmr))
--- test/jada/log_test.clj
+++ test/jada/log_test.clj
@@ -2,16 +2,38 @@
   (:require [jada.log :as log]
             [jada.food :as f]
             [clj-time.core :as t]
+            [monger.core :as mg]
+            [monger.collection :as mc]
             [clojure.test :refer :all]))
 
+(defn with-with-test-db [f]
+  "Connects and disconnects to a fresh test db."
+  (mg/connect!)
+  (mg/drop-db (mg/get-db "test"))
+  (mg/use-db! "test")
+  (f)
+  (mg/disconnect!))
+
+(defn with-temporary-entries [f]
+  "Snapshots the db, performs f, then loads the snapshot."
+  (let [all-records (mc/find-maps "test")]
+    (f)
+    (mc/remove "test")
+    (mc/insert-batch all-records)))
+
+(use-fixtures :once with-test-db)
+(use-fixtures :each with-temporary-entries)
 
 (def today (t/today-at-midnight))
 (def cookies (f/create "cookies" 1 2 3 4 5))
 
 (testing  "weight"
-  (deftest weight-is-updated
-    (is (= (:weight ((log/weight {} today 87) today)) 87))
-    (is (= (:weight ((log/weight {} 87) today)) 87))))
+  (deftest weight-with-date
+    (log/weight 87)
+    (is (= (:weight (log/today)) 87)))
+  (deftest weight-without-date
+    (log/weight today 87)
+    (is (= (:weight (log/today)) 87))))
 
 (testing "ate"
   (deftest food-is-appended
