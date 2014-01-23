779f3bc712094965e2a7d775d4060d4d24db30a5
expez <expez@expez.com>
2014-01-23 14:48:34 +0100

    food-list html

--- src/jada/html.clj
+++ src/jada/html.clj
@@ -1,8 +1,11 @@
 (ns jada.html
   (:require [hiccup.core :refer :all]
-            [hiccup.page :refer :all]))
+            [hiccup.page :refer :all]
+            [hiccup.element :refer :all]
+            [hiccup.form :refer :all]
+            [jada.food :as food]))
 
-(declare index-body head weight-form weight food-form)
+(declare index-body head weight-form weight food-form food-list)
 
 (defn index-page []
   (html5
@@ -15,34 +18,54 @@
     [:h1 "Jada"]
     (weight-form nil)
     (food-form)
+    (food)
     (include-js "js/jada.js")]))
 
 (defn- head []
   (html
    [:head
     [:title "Jada"]
-    (include-css "//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css")
+    (include-css "//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css"
+                 "css/jada.css")
     (include-js "http://code.jquery.com/jquery-2.0.3.min.js"
                 "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js"
                 "//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js")]))
 
 (defn- weight-form [val]
   (html
-   [:form {:id "weight-form"}
+   [:form {:id "weight-form" :role "form" :class "form-inline"}
     (weight val)
-    ]))
+    [:input {:type "submit" :value "Set weight" :class "btn btn-default"}]]))
 
-(defn weight [val]
+(defn- input-field [id label & [val type]]
+  [:div {:class "form-group"}
+   [:label {:for id} label]
+   [:input {:id id}]])
+
+(defn- weight [val]
   (html
-   "Weight:" [:input {:type "text" :value val :id "weight"}]))
+   (input-field "weight" "Weight: " val "text")))
 
 (defn- food-form []
   (html
-   [:form {:id "food-form"}
-    "Name:" [:input {:id "food-name"}]
-    "Kcal:" [:input {:id "food-kcal"}]
-    "Prot:" [:input {:id "food-prot"}]
-    "Fat:" [:input {:id "food-fat"}]
-    "Carbs:" [:input {:id "food-carbs"}]
-    "Fiber:" [:input {:id "food-fiber"}]
-    [:input {:type "submit" :value "Add food"}]]))
+   [:form {:id "food-form" :role "form" :class "form-inline"}
+    (input-field "food-name" "Name: ")
+    (input-field "food-kcal" "Kcal: ")
+    (input-field "food-prot" "Prot: ")
+    (input-field "food-fat" "Fat: ")
+    (input-field "food-carbs" "Carbs: ")
+    (input-field "food-fiber" "Fiber: ")
+    [:input {:type "submit" :value "Add food" :class "btn btn-default"}]]))
+
+(defn food []
+  [:div {:id "food"}
+   (food-list)
+   (food-item)])
+
+(defn- food-list []
+  [:div {:id "food-list"}
+   (unordered-list (map  :name (food/list-all)))])
+
+(defn- food-item []
+  (label "food-item-kcal" "Kcal")
+  [:span {:id "food-item-kcal"}])
