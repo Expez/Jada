c8fe36d49a32c65c26868debf5d9ebb0c3357722
expez <expez@expez.com>
2014-02-11 13:02:41 +0100

    {{outlet}} for displaying food now works

---
+++ resources/js/controllers/foodsController.js
@@ -0,0 +1,4 @@
+App.FoodsController = Ember.ArrayController.extend({
+  sortProperties: ['name'],
+  sortAscending: false
+});
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,23 @@
 App.Router.map(function() {
-  this.route("food");
+  // this.route("food");
+  this.resource('foods', function(){
+    this.resource('food', { path:'/:id' }, function(){
+      this.route('edit');
+    });
+    this.route('create');
+  });
   this.route("log", { path: "/log" });
 });
 
-App.FoodRoute = Ember.Route.extend({
+App.FoodsRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('food');
+  }
+});
+
+App.FoodRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.find('food', params.id);
   }
 });
 
